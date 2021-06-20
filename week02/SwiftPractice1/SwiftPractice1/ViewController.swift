7c14113c4448fe82288cdc8cb12bfd79696fe3bb
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-20 10:54:37 +0900

    week02 start

---
+++ week02/SwiftPractice1/SwiftPractice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPractice1
+//
+//  Created by 손대홍 on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
