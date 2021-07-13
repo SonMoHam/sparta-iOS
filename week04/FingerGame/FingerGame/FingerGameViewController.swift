255bd42aedbfb7c991095f0f2b2e6b8e271efa20
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-07-13 16:12:03 +0900

    finger game init

---
+++ week04/FingerGame/FingerGame/FingerGameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FingerGameViewController.swift
+//  FingerGame
+//
+//  Created by 손대홍 on 2021/07/13.
+//
+
+import UIKit
+
+class FingerGameViewController: UIViewController {
+
+    @IBOutlet weak var gameView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeButtonClicked(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
