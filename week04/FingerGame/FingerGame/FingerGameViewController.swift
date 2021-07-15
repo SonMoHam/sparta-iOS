d21012cf684900c18c6a2d8831535d66f5c4ef87
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-07-15 14:26:15 +0900

    timer, animation 추가

--- week04/FingerGame/FingerGame/FingerGameViewController.swift
+++ week04/FingerGame/FingerGame/FingerGameViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class FingerGameViewController: UIViewController {
     
     @IBOutlet weak var gameView: FingerGameView!
+    @IBOutlet weak var secondsLabel: UILabel!
+    
+    var secondsLeft = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +23,7 @@ class FingerGameViewController: UIViewController {
     
     
     @IBAction func closeButtonClicked(_ sender: Any) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     var timer: Timer?
     func touchCountDidChange() {
@@ -29,14 +32,64 @@ class FingerGameViewController: UIViewController {
             for roundView in self.gameView.touchToRoundView.values {
                 var randomChoice = Bool.random()
                 
-                if randomChoice {
-                    roundView.backgroundColor = UIColor(named: "myGreen")
-                } else {
-                    roundView.backgroundColor = UIColor(named: "myOrange")
+                UIView.animate(withDuration: 1) {
+                    if randomChoice {
+                        roundView.backgroundColor = UIColor(named: "myGreen")
+                    } else {
+                        roundView.backgroundColor = UIColor(named: "myOrange")
+                    }
+                    
+                    let center = roundView.center
+                    roundView.frame.size = CGSize(width: 90, height: 90)
+                    roundView.layer.cornerRadius = 45
+                    roundView.center = center
                 }
+                
             }
         }
+        resetSecondsTimer()
     }
+    
+    var secondsTimer: Timer?
+    func resetSecondsTimer() {
+        secondsTimer?.invalidate()
+        
+        if gameView.touchToRoundView.values.count > 0 {
+            //5초로 초기화
+            self.secondsLeft = 5
+            self.secondsLabel.text = "5"
+            self.blinkSecondsLabel()
+            
+            secondsTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (t) in
+                //1초 빼기
+                self.secondsLeft -= 1
+                //남은초 보여주기
+                self.secondsLabel.text = "\(self.secondsLeft)"
+                self.blinkSecondsLabel()
+                
+                //시간이 다되면
+                if self.secondsLeft == 0 {
+                    //라벨 텍스트 안 보이게
+                    self.secondsLabel.text = ""
+                    //초시계 타이머 무효화
+                    self.secondsTimer?.invalidate()
+                }
+            })
+            // 없으면 라벨 텍스트 안 보이게
+        } else {
+            self.secondsLabel.text = ""
+
+        }
+    }
+    
+    func blinkSecondsLabel() {
+        self.secondsLabel.alpha = 1
+        
+        UIView.animate(withDuration: 0.8) {
+            self.secondsLabel.alpha = 0
+        }
+    }
+    
     
     /*
      // MARK: - Navigation
